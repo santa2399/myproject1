a57e8a301bf7f34c3265893961caf659f0a3d6ab
santa2399 <124044017+santa2399@users.noreply.github.com>
2024-04-16 15:49:10 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","santa","hos");
+    $database= new mysqli("localhost","root","Santa@123","hos");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
